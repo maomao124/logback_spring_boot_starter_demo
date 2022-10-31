6dd7936774a2d2c16af82def600b3efcb30aec57
mao <1296193245@qq.com>
2022-10-31 22:25:50 +0800

    logback_spring_boot_starter_demo

---
+++ tools-log/src/main/java/mao/tools_log/exception/code/BaseExceptionCode.java
@@ -0,0 +1,21 @@
+package mao.tools_log.exception.code;
+
+/**
+ *
+ */
+public interface BaseExceptionCode
+{
+    /**
+     * 异常编码
+     *
+     * @return
+     */
+    int getCode();
+
+    /**
+     * 异常消息
+     *
+     * @return
+     */
+    String getMsg();
+}
